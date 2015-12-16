5921dd3ae08c90aa546143f787635984d0605802
philipbreau <philip.breau@icesoft.com>
2015-12-16 12:05:35 -0400

    delete user

--- app/pods/secure/realms/users/index/controller.js
+++ app/pods/secure/realms/users/index/controller.js
@@ -1,6 +1,50 @@
 import BaseController from 'console/controllers/base-controller';
-//import Ember from 'ember';
+import Ember from 'ember';
+import utils from 'console/helpers/utils';
 
 export default BaseController.extend({
+
+  selectedUser: null,
+
+  actions: {
+    openUserInfoModal: function(user){
+      this.set('selectedUser', user);
+      Ember.$('#userInfoModal').modal();
+    },
+    closeUserInfoModal: function(){
+      this.set('selectedUser', null);
+      Ember.$('#userInfoModal').modal('hide');
+    },
+    openConfirmDeleteUserPopup: function() {
+      Ember.$('#confirmDeleteUserModal').modal();
+    },
+    closeConfirmDeleteUserPopup: function() {
+      Ember.$('#confirmDeleteUserModal').modal('hide');
+    },
+    confirmDeleteUser: function(user){
+      this.set('selectedUser', user);
+      this.send('openConfirmDeleteUserPopup');
+    },
+    cancelDeleteUser: function(){
+      this.send('closeConfirmDeleteUserPopup');
+      this.set('selectedUser', null);
+    },
+    deleteUserConfirmed: function(){
+
+      var user = this.get('selectedUser');
+
+      bridgeit.io.admin.deleteRealmUser({username: user.get('username')}).then(() => {
+        var realm = this.get('model');
+        realm.get('users').removeObject(user);
+        this.get('toast').info('Successfully deleted user ' + user.get('fullname'));
+        this.send('closeConfirmDeleteUserPopup');
+        this.set('selectedUser',null);
+      }).catch((error) => {
+        console.error('deleteUser error: ' + error);
+        var errorMessage = utils.extractErrorMessage(err);
+        this.get('toast').error(error);
+      });
+    },
+  }
 
 });
