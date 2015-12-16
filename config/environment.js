800d986d8604ddeeb784a1d14cbd6bab3f23f671
philipbreau <philip.breau@icesoft.com>
2015-12-16 14:09:26 -0400

    toastr options

--- config/environment.js
+++ config/environment.js
@@ -35,19 +35,20 @@ module.exports = function(environment) {
     toastrOptions: {
       closeButton: true,
       debug: false,
-      newestOnTop: true,
-      progressBar: true,
+      newestOnTop: false,
+      progressBar: false,
       positionClass: 'toast-bottom-left',
-      preventDuplicates: true,
+      preventDuplicates: false,
       onclick: null,
       showDuration: '300',
       hideDuration: '1000',
-      timeOut: '4000',
-      extendedTimeOut: '1000',
+      timeOut: '0',
+      extendedTimeOut: '0',
       showEasing: 'swing',
       hideEasing: 'linear',
       showMethod: 'fadeIn',
-      hideMethod: 'fadeOut'
+      hideMethod: 'fadeOut',
+      tapToDismiss: false
     }
   };
 
