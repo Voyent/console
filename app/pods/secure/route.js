023b4d153bf71513f9aa5453f09e27762722e5cb
philipbreau <philip.breau@icesoft.com>
2016-06-08 11:15:21 -0300

    add session expiry handler

--- app/pods/secure/route.js
+++ app/pods/secure/route.js
@@ -6,5 +6,11 @@ export default BaseRoute.extend({
 			this.info('unauthorized, transitioning to index');
 			this.transitionTo('index');
 		}
+		else{
+			window.addEventListener('bridgeit-session-expired', (e) => {
+				console.log('console app received event bridgeit-session-expired', e);
+				this.transitionTo('index');
+			});
+		}
 	}
 });
